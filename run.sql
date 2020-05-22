3a7aedc9d7e58a6820ed3adb7c06b80187e8aeca
Alyona <44830907+AlliFrum@users.noreply.github.com>
2020-05-22 23:08:59 +0300

    Update run.sql

--- run.sql
+++ run.sql
@@ -4,8 +4,9 @@ SELECT * FROM TABLE(HEROES_PKG.HERO_INFO(0, 'Boba'));
 SELECT * FROM TABLE(HEROES_PKG.HERO_INFO(0, 'Bob'));
 SELECT * FROM TABLE(HEROES_PKG.HERO_INFO(700, 'Bob'));
 
+
 BEGIN 
-DELETE_UNIT ('Bob', 'Cyclops');
-DELETE_UNIT ('Boba', 'Halberdier'); --Вызовет ошибку, но так и запланировано)))
-DELETE_UNIT ('Bob', 'Gorgon');
+HEROES_PKG.DELETE_UNIT ('Bob', 'Cyclops');
+HEROES_PKG.DELETE_UNIT ('Boba', 'Halberdier'); --will give an error, as planned)))
+HEROES_PKG.DELETE_UNIT ('Bob', 'Gorgon');
 END;
